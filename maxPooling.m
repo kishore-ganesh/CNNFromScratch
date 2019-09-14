7d504d09f75489fcabf558a048ddc69b9d5d9f34
kishore-ganesh <kishore2912000@gmail.com>
2019-09-14 19:42:28 +0530

    Initial commit
    Signed-off-by: kishore-ganesh <kishore2912000@gmail.com>

---
+++ maxPooling.m
@@ -0,0 +1,20 @@
+function [output] = maxPooling(input, poolingMatrix)
+  xSize = size(poolingMatrix)(1);
+  ySize = size(poolingMatrix)(2);
+  inputSizeX = size(input)(1)
+  inputSizeY = size(input)(2)
+  %Check for odd
+  output = zeros(floor(inputSizeX/xSize), floor(inputSizeY/ySize));
+  for x = 1:xSize:size(input)(1)
+    for y = 1:ySize:size(input)(2)
+      rangeXL = x;
+      rangeXR = rangeXL + xSize - 1;
+      rangeYL = y;
+      rangeYR = rangeYL + ySize -1;
+      output(floor(x/xSize)+1, floor(y/ySize)+1) = max(max(input(rangeXL:rangeXR, rangeYL:rangeYR)));            
+    end
+    end
+end
+
+
+
